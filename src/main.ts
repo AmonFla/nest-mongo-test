f3c0340a57728ce414105cdc569d8870fb5fb093
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-14 14:10:54 -0300

    swagger configured and resources created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('MongDB Book REST API')
+    .setDescription('API REST')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // Path for Swagger docs
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
