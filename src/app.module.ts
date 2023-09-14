4f6250603a772d35d9deda8875f5ae9ec2861833
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-14 14:17:37 -0300

    moongo added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BooksModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/tutorial'),
+    BooksModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
