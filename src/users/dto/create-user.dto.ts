3970975b32f61fff29be940a11e77ec38698c81d
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-15 16:30:50 -0300

    user module base configuration

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'jhondoe' })
+  readonly _id: string;
+
+  @ApiProperty({ example: 'Json Doe' })
+  readonly name: string;
+
+  @ApiProperty({ example: 'jhondow@gmail.com' })
+  readonly email: string;
+
+  @ApiProperty({ example: 'Spain' })
+  readonly country: string;
+}
